021deefc3317364262977bd9a9377d9c78c5e58e
SebasBazauri <86888189+SebasBazauri@users.noreply.github.com>
2025-12-10 01:14:02 -0500

    Remove CORS configuration from application class

--- src/main/java/com/inso_final/CRUD_ALMACENERO/CrudAlmaceneroApplication.java
+++ src/main/java/com/inso_final/CRUD_ALMACENERO/CrudAlmaceneroApplication.java
@@ -9,19 +9,4 @@ public class CrudAlmaceneroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudAlmaceneroApplication.class, args);
 	}
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
-}
-
 }
